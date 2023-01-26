2f5dd1d6599910801f41c4f042c3626cecb73e26
carraes <carraeshb@gmail.com>
2023-01-25 23:25:36 -0300

    lint fix

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { wait } from '@testing-library/user-event/dist/utils';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
 import UserProvider from '../context/UserProvider';
